f9676b0b193e2e0b79de9d819d29af5cbdd8862f
peter <peter.g.hristov@gmail.com>
2020-07-14 19:44:40 +0100

    Initial varni bot working.

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ client.once('ready', () => {
 client.on('message', message => {
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-    message.channel.send("Malchi s tva " + message.content + ' ami varvi bluskai!!!');
+    const args = message.content.slice(config.prefix.length).trim()
+    message.channel.send('Malchi s tva "' + args + '" ami varvi bluskai!!!');
 });
 
 //console.log(process.env.BOT_TOKEN)
